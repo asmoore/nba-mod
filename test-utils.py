4a9d5ac39bc5d731ef43135de4b5fb3d4334de42
Andrew Moore <asmoor@gmail.com>
2014-10-25 08:32:38 -0400

    test standings

--- test-utils.py
+++ test-utils.py
@@ -13,21 +13,21 @@ import unittest
 import utils
 
 class UtilsTest(unittest.TestCase):
-    def test_get_team_subreddits(self):
-    	utils.get_team_subreddits(3)
+    #def test_get_team_subreddits(self):
+    #	utils.get_team_subreddits(3)
 
 
-    def test_get_schedule(self):
-    	utils.get_schedule(3)
+    #def test_get_schedule(self):
+    #	utils.get_schedule(3)
     
 
-    def test_get_game_threads(self):
-    	utils.get_game_threads()
+    #def test_get_game_threads(self):
+    #	utils.get_game_threads()
 
 
     def test_get_standings(self):
-    	utils.get_standings()
-
+    	standings = utils.get_standings()
+    	print standings
 
 if __name__ == '__main__':
     unittest.main()
